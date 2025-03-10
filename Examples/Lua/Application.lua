35221a18bf6a9188cbf4cccd365eda7902d8541c
Arda Bozdere <ardabozdere2010@gmail.com>
2025-03-10 17:31:58 +0000

    Update Application.lua

--- Examples/Lua/Application.lua
+++ Examples/Lua/Application.lua
@@ -2,4 +2,5 @@ sunflower.init("Window Name!", 1000, 1000)
 print("You can oviously do any lua codes")
 sunflower.graphics.color(255, 100, 50, 255)
 
+-- Simply makes it so when the app crashes the cmd only crashes on a key input
 sunflower.debugger.pauseOnCrash = true
